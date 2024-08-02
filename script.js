876872c78a396c90a514460353cd56c60fcbf118
Tony Tran <64768038+ttran97@users.noreply.github.com>
2024-08-02 15:05:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Allows to change different words 
+var typed = new Typed(".multiple-text", {
+    strings: ["Cybersecurity Engineer", "Application Security Engineer"],
+    typeSpeed: 100, 
+    backSpeed: 100,
+    backDelay: 1000, 
+    loop: true
+})
